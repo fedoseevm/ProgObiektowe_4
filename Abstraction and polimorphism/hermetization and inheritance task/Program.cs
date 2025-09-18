19a34873e8fce36f6fe05546cf6b044afc2cf3e0
Maksim Fedoseev <114389643+fedoseevm@users.noreply.github.com>
2025-09-18 15:08:53 +0200

    Add files via upload

---
+++ Abstraction and polimorphism/hermetization and inheritance task/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    class Room
+    {
+        private int roomNumber;
+        private int numberOfBeds;
+
+        public Room(int roomNumber, int numberOfBeds)
+        {
+            this.roomNumber = roomNumber;
+            this.numberOfBeds = numberOfBeds;
+        }
+
+        public int getRoomNumber() { return roomNumber; }
+        public int getNumberOfBeds() { return numberOfBeds; }
+    }
+
+    class StandardRoom : Room
+    {
+        private bool hasView = false;
+
+        public StandardRoom(int roomNumber, int numberOfBeds, bool hasView) : base(roomNumber, numberOfBeds)
+        {
+            this.hasView = hasView;
+        }
+    }
+
+    class Apartment : Room
+    {
+        private int apartmentNumber;
+        private int floor;
+
+        public Apartment(int roomNumber, int numberOfBeds, int apartmentNumber, int floor) : base(apartmentNumber, floor)
+        {
+            this.apartmentNumber = apartmentNumber;
+            this.floor = floor;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Apartment a = new Apartment(73, 2, 3, 23);
+        }
+    }
+}
