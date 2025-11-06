19e14cd6f55e808e1e829f0989f23f6f6a011a14
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-06 03:09:39 +0100

    Add new classes

---
+++ Programming patterns/FlightsReservation/classes/LoginManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightsReservation.classes
+{
+    internal class LoginManager
+    {
+        private static LoginManager _instance;
+        private LoginManager() { }
+
+        public static LoginManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new LoginManager();
+            }
+            return _instance;
+        }
+
+
+    }
+}
---
+++ Programming patterns/FlightsReservation/classes/Reservation.cs
@@ -0,0 +1,9 @@
+namespace ReservationSystem
+{
+    public class Reservation
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Location { get; set; }
+    }
+}
