c3a11a4481ffe755f92a489bd83b11d895f98389
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-17 19:56:04 +0100

    Replace Reservation.cs with FlightReservation.cs

--- Programming patterns/FlightsReservation/Classes/Models/Reservation.cs
+++
@@ -1,9 +0,0 @@
-namespace ReservationSystem
-{
-    public class Reservation
-    {
-        public string Name { get; set; }
-        public string Date { get; set; }
-        public string Location { get; set; }
-    }
-}
