ac16785283df12b901b8f2c759e8dbc8471c1ec1
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-19 23:30:01 +0100

    Add ReservationDirector functionality

--- Programming patterns/FlightsReservation/Classes/Builder/ReservationDirector.cs
+++ Programming patterns/FlightsReservation/Classes/Builder/ReservationDirector.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightsReservation.Classes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace FlightsReservation.Classes.Builder
 {
     internal class ReservationDirector
     {
+        public FlightReservation BuildReservation(IReservationBuilder builder, UserInput userInput)
+        {
+            builder.setFlightNumber(userInput.FlightNumber);
+            builder.setFrom(userInput.From);
+            builder.setTo(userInput.To);
+            builder.setDepartureDate(userInput.DepartureDate);
+            builder.setArrivalDate(userInput.ArrivalDate);
+            builder.setPrice(userInput.Price);
+            builder.setIsInternational();
+            builder.setPassportNumber(userInput.PassportNumber);
+            builder.setIsVisaRequired(userInput.IsVisaRequired);
+            return builder.Build();
+        }
     }
 }
